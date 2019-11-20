ff7775bd73e7ddba96c33e20e3dc22bf044f7eea
Zoe <Zoe.Stalkey@gmail.com>
2019-11-20 19:59:04 +0800

    Models set up in the database and API set up.

--- recipefinder/recipefinderApp/views.py
+++ recipefinder/recipefinderApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def IndexView(request):
+    url = 'https://api.spoonacular.com/recipes/search?apiKey=3dc537f85d054e38a4caadd57f887609&number=5'
+    
+    r = requests.get(url.format()).json()
+    # r is short for response
+
+    recipe_list = {
+        'title' : r['results'][4]['title'],
+        'image' : r['results'][4]['image'],
+        'servings': r['results'][4]['servings'],
+        'time' : r['results'][4]['readyInMinutes'],
+    }
+
+    context = {'recipe_list' : recipe_list}
+
+    return render(request, 'recipefinderApp/recipefinder.html', context)
+

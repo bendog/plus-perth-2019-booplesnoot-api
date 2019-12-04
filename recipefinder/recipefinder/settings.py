91cc218a4ffbb6a10e6c379cc882db94c5d5e90b
Beverly Oh <beverlyoh@gmail.com>
2019-12-04 20:15:23 +0800

    change database for dev

--- recipefinder/recipefinder/settings.py
+++ recipefinder/recipefinder/settings.py
@@ -78,14 +78,20 @@ WSGI_APPLICATION = "recipefinder.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "recipefinder",  # you will need to create this db
+#         "USER": "postgres",  # enter your user name here
+#         "PASSWORD": "poppies8",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "recipefinder",  # you will need to create this db
-        "USER": "postgres",  # enter your user name here
-        "PASSWORD": "poppies8",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
